374518cdbd00f6e33e49a7826ed8d6532dfc0e6c
BetulCengiz <bc199911@hotmail.com>
2021-01-08 22:32:14 +0300

    İlk yayın

--- Harmonik Toplam.py
+++ Harmonik Toplam.py
@@ -1,1 +1,15 @@
-print("harmoniktoplam")
+def harmonik_toplam(n):
+    if n == 1:
+        return 1
+    else:
+        return 1/n + harmonik_toplam(n-1)
+
+
+def harmoniktoplamiteratif(n):
+    toplam = 0
+    for i in range (1, n+1):
+        toplam = toplam + 1
+    return  toplam
+
+print(harmonik_toplam(4))
+print(harmoniktoplamiteratif(4))
